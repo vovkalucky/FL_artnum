48a5e7dfd1e580a7033d3a6dc23fdee50d2545a4
vladimir <vladimir.inves@gmail.com>
2021-12-15 20:27:24 +0300

    2

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ from datetime import datetime
 from selenium import webdriver
 #from seleniumwire import webdriver
 from selenium import webdriver
-from fake_useragent import UserAgent
+from selenium.webdriver.common.by import By
+import urllib
 from selenium.webdriver.common.keys import Keys
 #from selenium.webdriver.chrome.service import Service
 import time
@@ -30,18 +31,30 @@ def get_content():
     options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
 
     driver = webdriver.Chrome(
-        executable_path=r"C:\Users\Vladimir\PycharmProjects\FL_artnum\chromedriver.exe",
+        executable_path=r"/home/vladimir/PycharmProjects/FL_artnum/chromedriver",
         options=options
     )
     try:
         driver.get(url=URL)
         time.sleep(3)
-        find_input = driver.find_element("gLFyf gsfi")
-        #find_input = driver.find_element(value='[title="Поиск"]') #value = '[name="%s"]'
-        find_input.send_keys("8809647114819")
-        # find_input.send_keys(Keys.ENTER)
+        #find_input = driver.find_element("gLFyf gsfi")
+
+        find_input = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[2]/div[2]/input')
+        artikul = "8809647114819"
+        find_input.send_keys(artikul)
+        find_input.send_keys(Keys.ENTER)
+        time.sleep(2)
+        google_img = driver.find_element(By.XPATH, '//*[@id="hdtb-msb"]/div[1]/div/div[4]/a').click()
+        img = driver.find_element(By.TAG_NAME, 'img')
+        src = img.get_attribute('src')
+        print(src)
+        #driver.save_screenshot(f"{artikul}.png")
+        #src = img.get_attribute('src')
+        #urllib.urlretrieve(src, f"{artikul}.png")
+
+
         #driver.refresh() обновление окна браузера
-        time.sleep(100)
+        time.sleep(15)
     except Exception as ex:
         print(ex)
     finally:
